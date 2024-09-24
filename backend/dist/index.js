54efb3b4daba492c405a60baad3e1e8f89fb31ce
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-09-24 13:53:58 +0600

    inital commit

---
+++ backend/dist/index.js
@@ -0,0 +1,2 @@
+console.log("Starting the server..."); // This should print immediately
+console.log("---------------555-----5----66--------");
---
+++ backend/src/index.ts
@@ -0,0 +1,5 @@
+console.log("Starting the server..."); // This should print immediately
+console.log("---------------555-----5----66--------");
+
+
+
